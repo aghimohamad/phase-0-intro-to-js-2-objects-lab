9a6216a3c1f8cff452e9042b320a97ee0cac2142
mohamad <aghi.mohamad@gmail.com>
2022-04-10 02:32:08 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name : 'aghi',
+    streetAddress: '2310',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newo = {...employee};
+      newo[key] = value;
+      return newo;
+}
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+      employee[key] = value;
+      return employee;
+}
+function   deleteFromEmployeeByKey(employee, key){
+    const newo = {...employee};
+     delete newo[key];
+      return newo;
+}
+function   destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+      delete employee[key];
+      return employee;
+} 
